13bb46bd927a0502878f386fdc7a54f86806010a
Bowen Li <bwlee.sky@gmail.com>
2013-04-24 11:40:18 +0800

    Update auto-complete-settings.el

--- .emacs.d/settings/auto-complete-settings.el
+++ .emacs.d/settings/auto-complete-settings.el
@@ -4,7 +4,7 @@
 
 (add-to-list 'load-path "~/.emacs.d/auto-complete-1.3.1")
 (require 'auto-complete) 
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete-1.3.1/dict") ;; fix missing soft link
 (require 'auto-complete-config) 
 (ac-config-default)
 (global-auto-complete-mode t)
